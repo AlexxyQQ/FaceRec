41053e7e8023518f2914a5bc5a959e1892405758
AlexxyQQ <aayushpandey616@gmail.com>
2022-12-15 21:12:49 +0545

    line by line

---
+++ photoclicker.py
@@ -0,0 +1,133 @@
+# import time
+# import cv2
+# import dlib
+# import sys
+# import logging as log
+# import datetime as dt
+# import time 
+
+# cascPath = "haarcascade_frontalface_alt.xml"
+# faceCascade = cv2.CascadeClassifier(cascPath)
+# log.basicConfig(filename='webcam.log',level=log.INFO)
+
+
+# video_capture = cv2.VideoCapture(0)
+# anterior = 0
+# image_count = 0
+# while True:
+#     if not video_capture.isOpened():
+#         print('Unable to load camera.')
+#         time.sleep(5)
+#         pass
+
+#     # Capture image-by-image
+#     ret, image = video_capture.read()
+#     scale_percent = 80 # percent of original size
+#     width = int(image.shape[1] * scale_percent / 100)
+#     height = int(image.shape[0] * scale_percent / 100)
+#     dim = (width, height)
+    
+#     # Resize image
+#     image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#     faces = faceCascade.detectMultiScale(
+#         gray,
+#         scaleFactor=1.1,
+#         minNeighbors=5,
+#         minSize=(30, 30),
+#         flags = cv2.CASCADE_SCALE_IMAGE
+#     )
+
+#     # Draw a rectangle around the faces
+#     for (x, y, w, h) in faces:
+#         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#         save_face = image[y:y+h, x:x+w].copy()
+#         cv2.imwrite('dataset/aayush/{}.jpg'.format(image_count), save_face)
+
+
+#     if anterior != len(faces):
+#         anterior = len(faces)
+#         log.info(str(len(faces))+" face detected at "+str(dt.datetime.now()))
+#         image_count += 1    
+#     cv2.imshow('Video', image)
+
+
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+    
+#     # Display the resulting frame
+#     cv2.imshow('Video', image)
+
+    
+
+# # When everything is done, release the capture
+# video_capture.release()
+# cv2.destroyAllWindows()
+
+import time
+import cv2
+import dlib
+import sys
+import logging as log
+import datetime as dt
+import time 
+
+# cascPath = "haarcascade_frontalface_alt.xml"
+# faceCascade = cv2.CascadeClassifier(cascPath)
+
+
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+image_count = 0
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        time.sleep(5)
+        pass
+
+    # Capture image-by-image
+    ret, image = video_capture.read()
+    # scale_percent = 80 # percent of original size
+    # width = int(image.shape[1] * scale_percent / 100)
+    # height = int(image.shape[0] * scale_percent / 100)
+    # dim = (width, height)
+    
+    # Resize image
+
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # faces = faceCascade.detectMultiScale(
+    #     gray,
+    #     scaleFactor=1.1,
+    #     minNeighbors=5,
+    #     minSize=(30, 30),
+    #     flags = cv2.CASCADE_SCALE_IMAGE
+    # )
+
+    # Draw a rectangle around the faces
+    # for (x, y, w, h) in faces:
+    #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    #     save_face = image[y:y+h, x:x+w].copy()
+    cv2.imwrite('dataset/aaryan/{}.jpg'.format(image_count), image)
+
+
+    # if anterior != len(faces):
+    #     anterior = len(faces)
+    #     log.info(str(len(faces))+" face detected at "+str(dt.datetime.now()))
+    image_count += 1    
+    cv2.imshow('Video', image)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    # Display the resulting frame
+    cv2.imshow('Video', image)
+
+    
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
